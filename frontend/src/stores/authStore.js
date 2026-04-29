e2e289907e6248413d68f68231afba5119f4499f
yashaswikaahuja <yashaswikaahuja@users.noreply.github.com>
2026-04-29 16:14:47 +0000

    Remove stale JS files shadowing TS files

--- frontend/src/stores/authStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
-export const useAuthStore = create()(persist((set) => ({
-    accessToken: null,
-    setAccessToken: (accessToken) => set({ accessToken }),
-}), { name: 'auth-store' }));
